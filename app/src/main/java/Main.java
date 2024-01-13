5c5549d1ec0121674ad70b7dbc468566507e2985
Tom-akm <e235713@ie.u-ryukyu.ac.jp>
2024-01-13 20:03:46 +0900

    feat: Add GameController class

--- app/src/main/java/Main.java
+++ app/src/main/java/Main.java
@@ -2,17 +2,9 @@ import jp.ac.uryukyu.ie.e235713.*;
 
 public class Main {
     public static void main(String[] args) {
-        Player player1 = new Player("Hoge");
-        Bot bot1 = new Bot("Bot");
-        Game game = new Game(player1, bot1);
+        GameController gameController = new GameController();
 
-
-        game.addHand(player1);
-        player1.showHand();
-        game.addHand(bot1);
-        bot1.showHand();
-        game.frop();
-        game.showCommunityCard();
-        
+        gameController.startGame();
     }
+
 }
---
+++ app/src/main/java/jp/ac/uryukyu/ie/e235713/GameController.java
@@ -0,0 +1,42 @@
+package jp.ac.uryukyu.ie.e235713;
+
+public class GameController {
+    private Game game;
+    private Player player1;
+    private Bot bot1;
+
+    public GameController() {
+        this.player1 = new Player("Player");
+        this.bot1 = new Bot("Bot");
+        this.game = new Game(player1, bot1);
+    }
+
+    public void startGame() {
+        game.addHand(player1);
+        game.addHand(bot1);
+
+        for (int i = 0; i < 4; i++) {
+            player1.showHand();
+            game.playerAction();
+            game.checkWinner();
+            game.botAction();
+            game.checkWinner();
+
+            if (i == 0) {
+                game.frop();
+            } else {
+                game.turnAndRiver();
+            }
+
+            game.showCommunityCard();
+        }
+
+        System.out.println("Player's hand is :" + HandEvaluator.evaluateHand(player1, game));
+        System.out.println("Bot's hand is :" + HandEvaluator.evaluateHand(bot1, game) + "\n");
+        System.out.println(game.determineWinner() + "\n");
+        player1.showScore();
+        bot1.showScore();
+
+    }
+
+}
